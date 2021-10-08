57534ae2c94c2153308f5361994ae91b9adb2294
ליאל לוגסי <lielugasi@gmail.com>
2021-10-08 15:59:38 +0300

    HomeWork

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Products from './components/Products/Products';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,8 +19,11 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Header/>
+      <Products/>
     </div>
+    
   );
 }
 
---
+++ src/components/products/products.js
@@ -0,0 +1,23 @@
+import "./Products.css";
+import Product from "../Product/Product"; 
+
+function Products(){
+    return(
+        <section className="products">
+              
+        <Product/>
+        <Product/>
+        <Product/>
+        <Product/>
+        <Product/>
+        <Product/>
+        <Product/>
+        <Product/>
+        <Product/>
+        <Product/>
+       </section>
+     
+     
+   );  
+}
+export default Products;
