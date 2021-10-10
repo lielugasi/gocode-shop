f40f8539f0ac1117700bd282975b8aec0f576540
ליאל לוגסי <lielugasi@gmail.com>
2021-10-10 22:21:19 +0300

    Targil 2

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -1,17 +1,19 @@
 import "./Product.css";
 
-function Product(){
+function Product({id,title,price,description,category,image, rating}){
     return (
         
             <div className="product-card">
                 <div className="product-image">
-                  <img
-                    src="https://cdn.shopify.com/s/files/1/0938/8938/products/10231100205_1_1315x1800_300_CMYK_1024x1024.jpeg?v=1445623369"
-                  />
+                  <img src={image}/>
                 </div>
                 <div className="product-info">
-                  <h5>Winter Jacket</h5>
-                  <h6>$99.99</h6>
+                   {title}<br/>
+                    {price} <br/>
+                    {description}<br/>
+                     {category} <br/>
+                     {rating.rate}<br/>
+                      {rating.count}<br/>
                 </div>
               </div>
           );
--- src/components/products/products.js
+++ src/components/products/products.js
@@ -1,10 +1,21 @@
 import "./Products.css";
 import Product from "../Product/Product"; 
+import React from "react";
 
-function Products(){
+function Products({productsList}){
     return(
         <section className="products">
-              
+        {productsList.map((product)=>(
+          
+<Product key={product.id} id={product.id} title={product.title} price={product.price} description={product.description}
+         category={product.category} image={product.image} rating={product.rating}/>
+        
+           
+        ))}      
+        
+        
+        
+        {/* <Product/>
         <Product/>
         <Product/>
         <Product/>
@@ -12,9 +23,7 @@ function Products(){
         <Product/>
         <Product/>
         <Product/>
-        <Product/>
-        <Product/>
-        <Product/>
+        <Product/> */}
        </section>
      
      
